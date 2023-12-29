736391683ea5787a47a6c26b99baa7f19342ea3f
dropperDEV <jamescosta4747@Gmail.com>
2023-12-29 12:38:26 -0300

    feat: insight tabuleiro

---
+++ c++/insights/tabuleiro.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n = 8;
+    int tabuleiro[n][n];
+
+    for(int i =0; i < n; i++){
+        int inicio = 0;
+        if (i%2 == 0){
+            inicio = 0;
+        }
+        for (int j = 0; j < n; j++)
+        {
+            tabuleiro[i][j] = inicio;
+            if(inicio == 1){
+                inicio = 0;
+            } else{
+                inicio = 1;
+            }
+            
+        }
+        
+    }
+
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++)
+        {
+            cout << tabuleiro[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
