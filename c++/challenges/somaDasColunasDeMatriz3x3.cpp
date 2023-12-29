3a1ee4e95501aa9d137d0ce05d177e1fc07740d5
dropperDEV <jamescosta4747@Gmail.com>
2023-12-29 13:43:02 -0300

    feat: challenge somda das colunas de matrizes 3x3

---
+++ c++/challenges/somaDasColunasDeMatriz3x3.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Soma das Colunas de Matriz 3x3
+
+// Faça um programa para ler os elementos de uma matriz 3x3 e imprimir a soma de cada coluna da matriz.
+// Entrada
+
+// A entrada consiste de 9 linhas. Cada linha contém um inteiro. Os 9 inteiros representam uma matriz 3x3 onde os três primeiros inteiros representam os valores da primeira linha.
+// Saída
+
+// A saída consiste de três linhas. Cada linha contém uma mensagem indicando qual a soma dos elementos de determinada coluna.
+
+int main()
+{
+    int n = 3;
+    int vetor[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> vetor[i][j];
+        }
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+        int soma = 0;
+        for (int i = 0; i < n; i++)
+        {
+            soma += vetor[i][j];
+        }
+        cout << "Coluna " << j << ": " << soma << endl;
+    }
+
+    return 0;
+}
