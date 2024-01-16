4ee9e0ab8278446aabd0bf0407b94143bf38774c
dropperDEV <jamescosta4747@Gmail.com>
2024-01-16 11:39:28 -0300

    feat: queue stl

---
+++ c++/stl/queue.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define endl '\n';
+#define debug(x) cerr << #x << " = " << x << endl
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main(){
+    int n;
+    queue<int> fila;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int valor;
+       cin >> valor;
+       fila.push(valor);
+    }
+    fila.pop();
+   while(!fila.empty())
+    {
+        cout << fila.front() << '\n';
+        fila.pop();
+    }
+    
+
+    return 0;
+}
