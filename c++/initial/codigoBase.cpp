ac847bc3e0a1d9857a2263e94d095d637ea04be0
dropperDEV <jamescosta4747@Gmail.com>
2024-01-16 11:39:07 -0300

    feat: codigo base para desafios

---
+++ c++/initial/codigoBase.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define endl '\n';
+#define debug(x) cerr << #x << " = " << x << endl
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main(){
+    
+
+    return 0;
+}
